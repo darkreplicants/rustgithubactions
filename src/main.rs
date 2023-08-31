1f300663351394412ef07c879b3325c4cfb8a582
darkreplicants <darkreplicants@gmail.com>
2023-08-31 15:10:41 +0200

    created an stupid test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
+    cuadrado(5);
 }
+
+fn cuadrado(num: i32) -> i32{
+    num*num
+}
+
+
+// Don't mind this for now :)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sixteen_is_the_square_of_four() {
+        assert_eq!(16, cuadrado(4));
+    }
+
+}
+
